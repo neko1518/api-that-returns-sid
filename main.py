555ac19183623be504f36551d37bd04a387562f2
neko1517 <96658864+neko1518@users.noreply.github.com>
2022-09-06 10:22:37 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask,request,jsonify
+from flask import Flask,request
 import samino
 app = Flask(__name__)
 
@@ -9,5 +9,4 @@ def login():
   password = data["password"]
   device = data["device"]
   client = samino.Client(device)
-  x = client.login(email,password).json
-  return jsonify(x)
+  return client.login(email,password).sid
